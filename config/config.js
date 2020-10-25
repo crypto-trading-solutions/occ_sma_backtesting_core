5f074a0a79a04adbd20e4c48ee9f343b0e63f491
DmitriyShalaev <shalaevdmitriy99@gmail.com>
2020-10-25 15:52:42 +0200

    get fapi.binance.com klines && config

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    symbol : "BTCUSDT",
+    interval : "15m",
+    startDate: "2020-10-23",
+    endDate: "2020-10-24"
+}
--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 const axios = require('axios');
+const config = require('./config/config');
+const util = require('util');
 
+
+function dateToTimestamp(date) {
+  return new Date(date).getTime();
+}
+
+async function getBinanceData() {
+  try {
+    console.log(`CONFIG:${util.inspect(config)}`);
+
+    //  [0] - Open time | [1] - Open | [2] - High | [3] - Low | [4] - Close |
+    const response = await axios.get('https://fapi.binance.com/fapi/v1/klines', {
+      params: {
+        symbol: config.symbol,
+        interval: config.interval,
+        startTime: dateToTimestamp(config.startDate),
+        endTime: dateToTimestamp(config.endDate),
+        limit: 1500
+      }
+    });
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getBinanceData();
